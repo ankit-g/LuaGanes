c151c4874495e36f065e4df4adf8980e09be6cb3
ankit-g <gankyt@gmail.com>
2017-10-25 21:32:38 +0530

    full resolution mode added

--- games/main.lua
+++ games/main.lua
@@ -13,6 +13,49 @@ local tbl_clr = {}
 local Ball = require('ball')
 local fixed_balls = {}
 
+function love.conf(t)
+    t.identity = nil                    -- The name of the save directory (string)
+    t.version = "0.10.2"                -- The LÖVE version this game was made for (string)
+    t.console = false                   -- Attach a console (boolean, Windows only)
+    t.accelerometerjoystick = true      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
+    t.externalstorage = false           -- True to save files (and read from the save directory) in external storage on Android (boolean)
+    t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
+
+    t.window.title = "Untitled"         -- The window title (string)
+    t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
+    t.window.width = 1024                -- The window width (number)
+    t.window.height =  786              -- The window height (number)
+    t.window.borderless = false         -- Remove all border visuals from the window (boolean)
+    t.window.resizable = false          -- Let the window be user-resizable (boolean)
+    t.window.minwidth = 1               -- Minimum window width if the window is resizable (number)
+    t.window.minheight = 1              -- Minimum window height if the window is resizable (number)
+    t.window.fullscreen = false         -- Enable fullscreen (boolean)
+    t.window.fullscreentype = "desktop" -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
+    t.window.vsync = true               -- Enable vertical sync (boolean)
+    t.window.msaa = 0                   -- The number of samples to use with multi-sampled antialiasing (number)
+    t.window.display = 1                -- Index of the monitor to show the window in (number)
+    t.window.highdpi = false            -- Enable high-dpi mode for the window on a Retina display (boolean)
+    t.window.x = nil                    -- The x-coordinate of the window's position in the specified display (number)
+    t.window.y = nil                    -- The y-coordinate of the window's position in the specified display (number)
+
+    t.modules.audio = true              -- Enable the audio module (boolean)
+    t.modules.event = true              -- Enable the event module (boolean)
+    t.modules.graphics = true           -- Enable the graphics module (boolean)
+    t.modules.image = true              -- Enable the image module (boolean)
+    t.modules.joystick = true           -- Enable the joystick module (boolean)
+    t.modules.keyboard = true           -- Enable the keyboard module (boolean)
+    t.modules.math = true               -- Enable the math module (boolean)
+    t.modules.mouse = true              -- Enable the mouse module (boolean)
+    t.modules.physics = true            -- Enable the physics module (boolean)
+    t.modules.sound = true              -- Enable the sound module (boolean)
+    t.modules.system = true             -- Enable the system module (boolean)
+    t.modules.timer = true              -- Enable the timer module (boolean), Disabling it will result 0 delta time in love.update
+    t.modules.touch = true              -- Enable the touch module (boolean)
+    t.modules.video = true              -- Enable the video module (boolean)
+    t.modules.window = true             -- Enable the window module (boolean)
+    t.modules.thread = true             -- Enable the thread module (boolean)
+end
+
 math.random(os.time())
 local log = require 'log'
 log.outfile = 'game_log.c'
@@ -44,7 +87,8 @@ function get_ball_cordinates(x_res, y_res, radius)
 end
 
 function love.load(arg)
-  local ball_radius = 3
+  love.window.setMode(1366, 786, {resizable=true, vsync=false, minwidth=400, minheight=300})
+  local ball_radius = 20
   local num_rows, num_columns, ball_cordinates =
       get_ball_cordinates(love.graphics.getWidth(),love.graphics.getHeight(), ball_radius)
 
@@ -60,8 +104,7 @@ function love.load(arg)
                   count = count + 1
           end
   end
-  log.trace(#fixed_balls)
-  end
+end
 
 function love.draw()
   for ball_no = 1, #fixed_balls do
